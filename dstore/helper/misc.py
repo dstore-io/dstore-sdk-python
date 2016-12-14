18ad636d3a5bffa5ca06308cb01af62f47930d84
dstore-dbap <b.puttmann@dbap.de>
2016-12-14 14:45:36 +0100

    Use constants of metadata helper class

--- dstore/helper/misc.py
+++ dstore/helper/misc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import grpc
+from DstoreMetadata import DstoreMetadata
 
 def getChannel(server, port, pem_file_path, username=None, password=None, accesstoken='default'):
     # Build channel credentials. If file is not present, throw error.
@@ -9,9 +10,9 @@ def getChannel(server, port, pem_file_path, username=None, password=None, access
     channel_cerdentials = grpc.ssl_channel_credentials(pem_data)
     # Now also build call credentials. These will add the authtokens as metadata to each call.
     if username is not None and password is not None:
-        call_credentials = grpc.metadata_call_credentials(lambda context, callback: callback([('dstore-username', username),
-                                                                                              ('dstore-password', password),
-                                                                                              ('dstore-accesstoken', accesstoken)], None),
+        call_credentials = grpc.metadata_call_credentials(lambda context, callback: callback([(DstoreMetadata.USERNAME_KEY, username),
+                                                                                              (DstoreMetadata.PASSWORD_KEY, password),
+                                                                                              (DstoreMetadata.ACCESS_TOKEN_KEY, accesstoken)], None),
                                                           name='dstoreio_credentials')
         # Merge both credentials.
         channel_cerdentials = grpc.composite_channel_credentials(channel_cerdentials, call_credentials)
